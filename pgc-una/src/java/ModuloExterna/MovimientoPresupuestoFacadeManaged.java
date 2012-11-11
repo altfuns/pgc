8415724e5b47f7db23be064ceb3bdad6dc67d6d2
Luis Alfonso Aguilar <laltfuns@gmail.com>
2012-11-11 13:08:57 -0600

    Corrigiendo defecto que no dejaba agregar presupuestos de la UNA

--- pgc-una/src/java/ModuloExterna/MovimientoPresupuestoFacadeManaged.java
+++ pgc-una/src/java/ModuloExterna/MovimientoPresupuestoFacadeManaged.java
@@ -1,4 +1,3 @@
-
 package ModuloExterna;
 
 import Logs.Log;
@@ -20,7 +19,8 @@ import javax.servlet.http.HttpSession;
  *
  * @author Luis Alfonso Aguilar Sánchez
  */
-public class MovimientoPresupuestoFacadeManaged implements Logeable{
+public class MovimientoPresupuestoFacadeManaged implements Logeable {
+
     @EJB
     private PresupuestoFacadeRemote presupuestoFacade;
     @EJB
@@ -31,36 +31,34 @@ public class MovimientoPresupuestoFacadeManaged implements Logeable{
      */
     public MovimientoPresupuestoFacadeManaged() {
         tipos = new ArrayList<SelectItem>();
-        tipos.add(new SelectItem("001","Correos de Costa Rica"));
-        tipos.add(new SelectItem("002","Universidad Nacional"));
+        tipos.add(new SelectItem("001", "Correos de Costa Rica"));
+        tipos.add(new SelectItem("002", "Universidad Nacional"));
     }
 
     //<editor-fold defaultstate="collapsed" desc="Metodos">
+    private void seguridad() {
 
-    private void seguridad(){
-       
-        HttpSession session = (HttpSession)FacesContext.getCurrentInstance().getExternalContext().getSession(true);
-        SeguridadManagedBean seguridad = (SeguridadManagedBean)session.getAttribute("SeguridadManagedBean");
-        if(seguridad != null){
+        HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(true);
+        SeguridadManagedBean seguridad = (SeguridadManagedBean) session.getAttribute("SeguridadManagedBean");
+        if (seguridad != null) {
             seguridad.validarPermiso(codigoPermiso);
-        }
-        else{
-            try{
+        } else {
+            try {
                 FacesContext.getCurrentInstance().getExternalContext().redirect("/pgc-una/login.iface");
+            } catch (Exception e) {
             }
-            catch(Exception e){
-            }
-        }        
+        }
     }
+
     /**
      * Crea un nuevo Movimiento de presupuesto con los datos presentes en la pantalla
      */
-    public void guardar(){
+    public void guardar() {
         FacesContext context = FacesContext.getCurrentInstance();
         FacesMessage message = new FacesMessage();
         MovimientoPresupuesto mp = new MovimientoPresupuesto();
         Log l = new Log();
-        try{
+        try {
             mp.setFecha(movimientoPresupuestoLocal.getFecha());
             mp.setNumeroCheque(movimientoPresupuestoLocal.getNumeroCheque());
             mp.setImporte(movimientoPresupuestoLocal.getImporte());
@@ -68,15 +66,15 @@ public class MovimientoPresupuestoFacadeManaged implements Logeable{
             mp.setCodigo(movimientoPresupuestoLocal.getNumeroCheque() + movimientoPresupuestoLocal.getNumeroDeposito());
 
             Presupuesto p = presupuestoFacade.find(movimientoPresupuestoLocal.getPresupuesto());
-            if(manejarPresupuesto(p,mp)){
+            if (manejarPresupuesto(p, mp)) {
                 message.setSeverity(FacesMessage.SEVERITY_INFO);
                 message.setSummary("Movimiento agregado Correctamente");
                 context.addMessage("frm_principal", message);
                 listaMovimientos.add(movimientoPresupuestoLocal);
                 limpiarCampos();
             }
-            
-        }catch(Exception e){
+
+        } catch (Exception e) {
             message.setSeverity(FacesMessage.SEVERITY_ERROR);
             message.setSummary(e.getMessage());
             message.setDetail(e.getMessage());
@@ -84,6 +82,7 @@ public class MovimientoPresupuestoFacadeManaged implements Logeable{
             getLogFacade().error(this, e.getMessage());
         }
     }
+
     /**
      * Maneja el presupuesto al cual se le quiere ingresar el movimiento
      * @param p
@@ -93,12 +92,12 @@ public class MovimientoPresupuestoFacadeManaged implements Logeable{
      * @return
      * True si la operación se pudo realizar con exito, false en caso contrario
      */
-    public Boolean manejarPresupuesto(Presupuesto p, MovimientoPresupuesto mp){
-        if(movimientoPresupuestoLocal.getImporte() > 0){
+    public Boolean manejarPresupuesto(Presupuesto p, MovimientoPresupuesto mp) {
+        if (movimientoPresupuestoLocal.getImporte() > 0) {
             p.setMonto(p.getMonto() + movimientoPresupuestoLocal.getImporte());
-            if(p.getCodigo().equals("001")){
+            if (p.getCodigo().equals("001")) {
                 Presupuesto puna = presupuestoFacade.find("002");
-                if(movimientoPresupuestoLocal.getImporte() <= puna.getMonto()){
+                if (movimientoPresupuestoLocal.getImporte() <= puna.getMonto()) {
                     puna.setMonto(puna.getMonto() - movimientoPresupuestoLocal.getImporte());
                     presupuestoFacade.edit(puna);
                     auxiliarTransaction = puna;
@@ -111,8 +110,7 @@ public class MovimientoPresupuestoFacadeManaged implements Logeable{
                     movimientoPresupuestoFacade.create(mp);
                     getLogFacade().create(this);
                     return true;
-                }
-                else{
+                } else {
                     FacesContext context = FacesContext.getCurrentInstance();
                     FacesMessage message = new FacesMessage();
                     message.setSeverity(FacesMessage.SEVERITY_ERROR);
@@ -120,21 +118,23 @@ public class MovimientoPresupuestoFacadeManaged implements Logeable{
                     context.addMessage("frm_principal", message);
                     return false;
                 }
-            }
-            else{
-               presupuestoFacade.edit(p);
-               auxiliarTransaction = p;
-               getLogFacade().update(this);
-               auxiliarTransaction = null;
-               return true;
+            } else {
+                presupuestoFacade.edit(p);
+                auxiliarTransaction = p;
+                mp.setCodigoPresupuesto(p);
+                movimientoPresupuestoFacade.create(mp);
+                getLogFacade().update(this);
+                auxiliarTransaction = null;
+                return true;
             }
         }
         return false;
     }
+
     /**
      * Limpiar los campos de la pantalla
      */
-    public void limpiarCampos(){
+    public void limpiarCampos() {
         movimientoPresupuestoLocal.setFecha(null);
         movimientoPresupuestoLocal.setImporte(0);
         movimientoPresupuestoLocal.setNumeroCheque("");
@@ -142,21 +142,18 @@ public class MovimientoPresupuestoFacadeManaged implements Logeable{
 
     }
 
-
     //</editor-fold>
-
     //<editor-fold defaultstate="collapsed" desc="Log">
-    
     public String getInsertString() {
         MovimientoPresupuesto m = movimientoPresupuestoLocal;
         String query = "INSERT INTO movimiento_presupuesto(codigo, codigo_presupuesto, fecha, numero_deposito, numero_cheque, importe)" +
                 "VALUES ({0},{1},{2},{3},{4},{5});";
 
-        query = query.replace("{0}", m.getCodigo() == null? "" : m.getCodigo());
-        query = query.replace("{1}", m.getCodigoPresupuesto() == null? "" : m.getCodigoPresupuesto().getCodigo());
-        query = query.replace("{2}", m.getFecha() == null? "" : m.getFecha().toString());
-        query = query.replace("{3}", m.getNumeroDeposito() == null? "" : m.getNumeroDeposito());
-        query = query.replace("{4}", m.getNumeroCheque() == null? "" : m.getNumeroCheque());
+        query = query.replace("{0}", m.getCodigo() == null ? "" : m.getCodigo());
+        query = query.replace("{1}", m.getCodigoPresupuesto() == null ? "" : m.getCodigoPresupuesto().getCodigo());
+        query = query.replace("{2}", m.getFecha() == null ? "" : m.getFecha().toString());
+        query = query.replace("{3}", m.getNumeroDeposito() == null ? "" : m.getNumeroDeposito());
+        query = query.replace("{4}", m.getNumeroCheque() == null ? "" : m.getNumeroCheque());
         query = query.replace("{5}", String.valueOf(m.getImporte()));
 
         return query;
@@ -164,7 +161,7 @@ public class MovimientoPresupuestoFacadeManaged implements Logeable{
 
     public String getDeleteString() {
         MovimientoPresupuesto m = movimientoPresupuestoLocal;
-        
+
         String query = "DELETE FROM movimiento_presupuesto WHERE codigo = {0};";
         query = query.replace("{0}", m.getCodigo());
 
@@ -175,9 +172,9 @@ public class MovimientoPresupuestoFacadeManaged implements Logeable{
         Presupuesto m = auxiliarTransaction;
         String query = "UPDATE presupuesto SET titulo = {0}, monto = {1} WHERE codigo = {2};";
 
-        query = query.replace("{0}", m.getTitulo() == null? "" : m.getTitulo());
+        query = query.replace("{0}", m.getTitulo() == null ? "" : m.getTitulo());
         query = query.replace("{1}", String.valueOf(m.getMonto()));
-        query = query.replace("{2}", m.getCodigo() == null? "" : m.getCodigo());
+        query = query.replace("{2}", m.getCodigo() == null ? "" : m.getCodigo());
 
         return query;
     }
@@ -186,21 +183,19 @@ public class MovimientoPresupuestoFacadeManaged implements Logeable{
         return "movimiento_presupuesto";
     }
 
-    public  Empleado getEmpleado() {
-        HttpSession session = (HttpSession)FacesContext.getCurrentInstance().getExternalContext().getSession(true);
-        SeguridadManagedBean seguridad = (SeguridadManagedBean)session.getAttribute("SeguridadManagedBean");
+    public Empleado getEmpleado() {
+        HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(true);
+        SeguridadManagedBean seguridad = (SeguridadManagedBean) session.getAttribute("SeguridadManagedBean");
         return seguridad.getEmpleadoIngreso();
     }
 
-    private Log getLogFacade(){
-        HttpSession session = (HttpSession)FacesContext.getCurrentInstance().getExternalContext().getSession(true);
-        return (Log)session.getAttribute("Log");
+    private Log getLogFacade() {
+        HttpSession session = (HttpSession) FacesContext.getCurrentInstance().getExternalContext().getSession(true);
+        return (Log) session.getAttribute("Log");
     }
 
-     //</editor-fold>
-
+    //</editor-fold>
     //<editor-fold defaultstate="" desc="Propiedades y atributos">
-
     public ArrayList<SelectItem> getTipos() {
         return tipos;
     }
@@ -224,13 +219,14 @@ public class MovimientoPresupuestoFacadeManaged implements Logeable{
     public ArrayList<MovimientoPresupuestoLocal> getListaMovimientos() {
         //if(listaMovimientos.isEmpty()){
         seguridad();
-            listaMovimientos = new ArrayList<MovimientoPresupuestoLocal>();
-            ArrayList<MovimientoPresupuesto> aux = new ArrayList<MovimientoPresupuesto>(movimientoPresupuestoFacade.findAll());
-            for(MovimientoPresupuesto mp : aux){
-                if(movimientoPresupuestoLocal.getPresupuesto().compareTo(mp.getCodigoPresupuesto().getCodigo()) == 0)
-                    listaMovimientos.add(new MovimientoPresupuestoLocal(mp));
+        listaMovimientos = new ArrayList<MovimientoPresupuestoLocal>();
+        ArrayList<MovimientoPresupuesto> aux = new ArrayList<MovimientoPresupuesto>(movimientoPresupuestoFacade.findAll());
+        for (MovimientoPresupuesto mp : aux) {
+            if (movimientoPresupuestoLocal.getPresupuesto().compareTo(mp.getCodigoPresupuesto().getCodigo()) == 0) {
+                listaMovimientos.add(new MovimientoPresupuestoLocal(mp));
             }
-       // }
+        }
+        // }
 
         return listaMovimientos;
     }
@@ -242,14 +238,10 @@ public class MovimientoPresupuestoFacadeManaged implements Logeable{
     public Presupuesto getPresupuestoUNA() {
         return presupuestoFacade.find("002");
     }
-
-
     private String codigoPermiso = "16";
     private Presupuesto auxiliarTransaction;
-    private ArrayList<MovimientoPresupuestoLocal> listaMovimientos =  new ArrayList<MovimientoPresupuestoLocal>();
+    private ArrayList<MovimientoPresupuestoLocal> listaMovimientos = new ArrayList<MovimientoPresupuestoLocal>();
     private MovimientoPresupuestoLocal movimientoPresupuestoLocal = new MovimientoPresupuestoLocal();
     private ArrayList<SelectItem> tipos = new ArrayList();
     //</editor-fold>
-
-
 }
